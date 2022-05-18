d4aaa760de32e4b97e93071381d86e354112b996
Faizan Ansari <f97aizan@gmail.com>
2022-05-18 12:19:49 -0400

    updating the files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 
 
 const App = () => {
   return (
     <>
+     <Header />
+    <main>
+   
     <h1>Welcome To MyShop</h1>
-      
+    
+      </main>
+      <Footer/>
     </>
   );
 }
