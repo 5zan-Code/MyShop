b0fb76d2a5dc87eacfcb8138c69de0164dbfd5ec
Faizan Ansari <f97aizan@gmail.com>
2022-05-20 13:32:01 -0400

    Adding Rating comp and modifing Product comp

--- frontend/src/Components/Product.js
+++ frontend/src/Components/Product.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import {Card} from 'react-bootstrap'
+import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from '../Components/Rating'
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
   return (
-    <Card className= 'my-3 p-3 rounded '>
-       <a href={`/product/${product._id}`}>
-           <Card.Img src={product.image} variant='top'/>
-       </a>
+    <>
+      <Card className="my-3 p-3 rounded ">
+        <a href={`/product/${product._id}`}>
+          <Card.Img src={product.image} variant="top" />
+        </a>
 
-    </Card>
-  )
-}
+        <Card.Body>
+          <a href={`/product/${product._id}`}>
+            <Card.Title as="div">
+              <strong>{product.name}</strong>
+            </Card.Title>
+          </a>
 
-export default Product
+          <Card.Text>
+            <div className="my-3">
+            <Rating rating={product.Rating} numReviews ={`${product.numReviews} Reviews`} />
+          </div>
+          </Card.Text>
+          <Card.Text as="h3">${product.price}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 
+export default Product;
